cb8953f155ed55945704ff995be9941caf24e57b
Andrii Frunt <andrii.frunt@gmail.com>
2021-05-09 00:33:08 +0300

    init

--- ora2pg_conf_initializer.py
+++ ora2pg_conf_initializer.py
@@ -1,6 +1,10 @@
 import os
 import sys
+import time
 from os.path import exists
+
+
+def current_time_millis(): return round(time.time() * 1000)
 
 
 def only_lines_with_settings(lines):
@@ -27,13 +31,13 @@ def lines_to_settings(lines):
 def populate_env_values(settings):
     def choose_value(value, env_value): return value if env_value is None else env_value
 
-    def required_or_optional_value_supplied(optional, env_value):
+    def is_required_or_optional_value_supplied(optional, env_value):
         return not optional or (optional and env_value is not None)
 
     settings_with_env_values = [(key, value, optional, os.getenv(key)) for (key, value, optional) in settings]
 
     return [(key, choose_value(value, env_value)) for (key, value, optional, env_value) in
-            settings_with_env_values if required_or_optional_value_supplied(optional, env_value)]
+            settings_with_env_values if is_required_or_optional_value_supplied(optional, env_value)]
 
 
 def read_reference_settings(path):
@@ -46,7 +50,7 @@ def write_config_file(path, settings):
 
     with open(path, "w") as file:
         for (key, value) in settings:
-            print(f"{key} -> {value}")
+            print(f"{key} {value}")
             file.write(f"{key} {value}\n")
 
 
@@ -71,8 +75,12 @@ def initialize_ora2pg_conf(conf_location="/etc/ora2pg.conf", dist_conf_location=
 
 
 if __name__ == '__main__':
+    started_at = current_time_millis()
+
     if len(sys.argv) != 3:
         print("Locations of ora2pg.conf and ora2pg.conf.dist files should be specified as command-line arguments")
         exit(1)
 
     initialize_ora2pg_conf(conf_location=sys.argv[1], dist_conf_location=sys.argv[2])
+
+    print("Elapsed {}ms".format(current_time_millis() - started_at))
